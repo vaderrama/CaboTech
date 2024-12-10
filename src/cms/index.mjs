344b69b2a747706fb6a397cd3bac1df73e7a127c
vaderrama <juan.alvarezz.11@gmail.com>
2024-12-10 16:48:05 +0100

    first commit

---
+++ src/cms/index.mjs
@@ -0,0 +1,7 @@
+import { settings } from "./settings.mjs";
+import { page } from "./page.mjs";
+import { post } from "./post.mjs";
+import { project } from "./project.mjs";
+import { product } from "./product.mjs";
+
+export { settings, page, post, project, product };
